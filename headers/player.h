0aef486052761ad7d12333ab3b39a20e8ff92997
Nikkie Schaad <Nikkieschaad@gmail.com>
2022-12-06 18:21:14 +0100

    commit

--- headers/player.h
+++ headers/player.h
@@ -6,6 +6,7 @@ class playerClass
     private:
         saveGame saveObj;
         saveTypes::saveData load = saveObj.getSave();
+        bool moneyCheck(float currentWallet, float currentPrice);
     public:
         void eat();
         void rest();
--- player.cpp
+++ player.cpp
@@ -1,29 +1,50 @@
 #include "headers/player.h"
+#include "headers/menu.h"
+
+bool playerClass::moneyCheck(float currentWallet, float currentPrice)
+{
+    menuClass menuObj;
+
+    if (currentWallet < currentPrice)
+    {
+        std::cout << "-- You don't have enough money for this! --" << std::endl;
+        std::cout << "== Current wallet: " << this->load.wallet <<  " ==" << std::endl;
+        std::cout << "Work, steal, or gamble to earn more!" << std::endl;
+        std::cin.get();
+        menuObj.showHome();
+        return false;
+    } else return true;
+}
 
 void playerClass::eat()
 {
-    int increment;
-    if (this->load.hunger >= 300) increment = 0;
-    else increment = 15;
-    
-    this->load.hunger += increment;
-    this->load.health -= 5;
-    this->load.wallet -= 0.25;
-    
-    this->saveObj.save(this->load);
+    if (this->moneyCheck(this->load.wallet, 0.25))
+    {
+        int increment;
+        if (this->load.hunger > 299) increment = 0;
+        else increment = 15;
+        
+        this->load.hunger += increment;
+        this->load.health -= 5;
+        this->load.wallet -= 0.25;    
+        this->saveObj.save(this->load);
+    }
 }
 
 void playerClass::rest()
-{
-    int increment;
-    if (this->load.health >= 300) increment = 0;
-    else increment = 25;
+{   
+    if (this->moneyCheck(this->load.wallet, 0.25))
+    {
+        int increment;
+        if (this->load.health > 299) increment = 0;
+        else increment = 25;
 
-    this->load.health += increment;
-    this->load.hunger -= 5;
-    this->load.wallet -= 0.25;
-    
-    this->saveObj.save(this->load);
+        this->load.health += increment;
+        this->load.hunger -= 5;
+        this->load.wallet -= 0.25;
+        
+        this->saveObj.save(this->load);
+    }
 }
 void playerClass::steal()
 {
